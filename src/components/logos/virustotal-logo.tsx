99adf5ea5568c45e620c31f2747b83f1f3e4cfee
Hari Mathesh <harimathesh3@gmail.com>
2025-12-17 08:39:46 +0000

    use this for virustotal

--- src/components/logos/virustotal-logo.tsx
+++ src/components/logos/virustotal-logo.tsx
@@ -3,15 +3,12 @@ import type { FC, SVGProps } from 'react';
 export const VirusTotalLogo: FC<SVGProps<SVGSVGElement>> = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 256 256"
+    viewBox="0 0 24 24"
     width="1em"
     height="1em"
+    fill="#4263f5"
     {...props}
   >
-    <g>
-      <path d="M79.8 192.2L0 134.7l21.2-21.2l52.9 52.8l-15.5 25.9z" fill="#264A72" />
-      <path d="m100.3 216.3l21.2 21.2l134.5-134.5l-21.2-21.2z" fill="#396A9A" />
-      <path d="M213.5 0L100.3 113.2l42.4 42.4L256 42.4z" fill="#4EC5B1" />
-    </g>
+    <path d="M20 3H8l6 9-6 9h12V3z" />
   </svg>
 );
