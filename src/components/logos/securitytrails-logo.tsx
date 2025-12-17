9ef7909383009d525b698c84c7cb2f8f382752a7
Hari Mathesh <harimathesh3@gmail.com>
2025-12-17 08:32:23 +0000

    change these icons to these ones

--- src/components/logos/securitytrails-logo.tsx
+++ src/components/logos/securitytrails-logo.tsx
@@ -6,9 +6,13 @@ export const SecurityTrailsLogo: FC<SVGProps<SVGSVGElement>> = (props) => (
     viewBox="0 0 24 24"
     width="1em"
     height="1em"
-    fill="#3363FF"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     {...props}
   >
-    <path d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12c5.16-1.26 9-6.45 9-12V5l-9-3zm0 10.99h7c-.53 4.12-3.28 7.79-7 8.94V12H5V6.3l7-3.11v9.8z" />
+    <path d="M20 3H8l6 9-6 9h12" color="#3363FF" />
   </svg>
 );
